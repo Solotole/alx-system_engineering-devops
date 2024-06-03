1b7e8596cbce25d29416e04f0ec3d21bc9476277
Solotole <solomontoleak47@gmail.com>
2024-06-03 17:43:49 +0100

    retriving all and json represent all data

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" script to send request and get response
+    from an open source API of all users and converting
+    into json format
+"""
+if __name__ == '__main__':
+    import json
+    import requests
+
+    main_dict = {}
+    mini_dict = {}
+    lists = []
+    r = requests.get('https://jsonplaceholder.typicode.com/users')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    for user in r.json():
+        user_name = user.get('username')
+        user_id = user.get('id')
+        for task in todos.json():
+            tasks = task.get('title')
+            status = task.get('completed')
+            mini_dict['username'] = user_name
+            mini_dict['task'] = tasks
+            mini_dict['completed'] = status
+            lists.append(mini_dict)
+            mini_dict = {}
+        main_dict[user_id] = lists
+        lists = []
+
+    with open('todo_all_employees.json', 'w') as files:
+        json_object = json.dumps(main_dict)
+        files.write(json_object)
