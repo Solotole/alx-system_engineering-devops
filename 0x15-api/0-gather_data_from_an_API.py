e19694045121c962b481de471ed0312ffeea1d73
Solotole <solomontoleak47@gmail.com>
2024-06-03 13:41:10 +0100

    gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
     import sys
     completed = 0
     total = 0
-    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(sys.argv[1]))
     user = r.json().get('name')
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
     for task in todos.json():
@@ -16,7 +17,8 @@ if __name__ == '__main__':
             if task.get('completed'):
                 completed += 1
 
-    print('Employee {} is done with tasks ({}/{}):'.format(user, completed, total))
+    print('Employee {} is done with tasks ({}/{}):'
+          .format(user, completed, total))
     for task in todos.json():
         if task.get('completed') and task.get('userId') == int(sys.argv[1]):
             print('\n'.join(["\t " + task.get('title')]))
