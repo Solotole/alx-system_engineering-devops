d64651dea9dbcedaa8e48af5b26451854582d48a
Solotole <solomontoleak47@gmail.com>
2024-06-18 11:35:50 +0100

    counting number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module that counts number of reddit subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ counting number of subscribers """
+    url = 'https://www.reddit.com/r/{}/about.json'
+    complete_url = url.format(subreddit)
+    headers = {'User-Agent': 'python'}
+    response = requests.get(complete_url,
+                            headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    elif response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
